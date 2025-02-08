3e3f19052399ebaca1a38afed13c89ffdd72add4
Soraru19 <narukyubin@gmail.com>
2025-02-07 19:58:26 -0600

    Gata bot

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ import { en, es, id, ar, pt, de, it } from './lib/idiomas/total-idiomas.js'
 // EN ➜ If you want to receive reports you must use the three parameters (Number, name and true)
 global.owner = [
 ['5214774444444', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌻🐈
-['593968263524', '𝙂𝘽 𝘼𝙨𝙞𝙨𝙩𝙚𝙣𝙘𝙞𝙖 🌻🐈', true],
+['593968263524', 'Owner (1) 💻', true],
 ['5218715746374'],
 ['5492266613038'], 
 ['595976126756'],
